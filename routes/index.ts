05d8a67dbcec5fdea20ed376a46338d39e361b07
RhazzXIX <jm21tonio@gmail.com>
2023-06-12 22:35:09 +0800

    Add initial messages

--- routes/index.ts
+++ routes/index.ts
@@ -1,11 +1,24 @@
 import { NextFunction, Request, Response } from "express";
 
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello Up-skilling World!",
+    user: "Driven Person",
+    added: new Date(),
+  },
+];
+
 /* GET home page. */
-router.get('/', function(req: Request, res: Response, next: NextFunction) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req: Request, res: Response, next: NextFunction) {
+  res.render("index", { title: "MiniMessageBoard", messages: messages });
 });
 
 module.exports = router;
