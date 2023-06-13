e2bd7f5107c5b93b1ebe7d332b2a1ecf80152e99
RhazzXIX <jm21tonio@gmail.com>
2023-06-13 14:35:41 +0800

    Add type message & logic to sort messages

--- routes/index.ts
+++ routes/index.ts
@@ -3,22 +3,33 @@ import { NextFunction, Request, Response } from "express";
 const express = require("express");
 const router = express.Router();
 
+type message = {
+  text: string;
+  user: string;
+  added: Date;
+};
+
 const messages = [
   {
     text: "Hi there!",
     user: "Amando",
-    added: new Date(),
+    added: new Date(1686629585892),
   },
   {
     text: "Hello Up-skilling World!",
     user: "Driven Person",
-    added: new Date(),
+    added: new Date(1686629623383),
   },
 ];
 
 /* GET home page. */
 router.get("/", function (req: Request, res: Response, next: NextFunction) {
-  res.render("index", { title: "MiniMessageBoard", messages: messages });
+  res.render("index", {
+    title: "Mini Message Board",
+    messages: messages.sort((a: message, b: message) => {
+      return b.added.getTime() - a.added.getTime();
+    }),
+  });
 });
 
 module.exports = { router, messages };
