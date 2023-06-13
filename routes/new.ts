e8d3742bbdedba57464dede68efa4bdbcc649c05
RhazzXIX <jm21tonio@gmail.com>
2023-06-13 11:21:48 +0800

    Add message form

--- routes/new.ts
+++ routes/new.ts
@@ -1,12 +1,11 @@
 import { Handler } from "express";
 
-
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  res.render("form");
 } as Handler);
 
 module.exports = router;
